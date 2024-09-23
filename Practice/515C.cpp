ba2942682d9b596684789b6966dea7b879f12666
Angshuman Sengupta <angshuman@cse.iitb.ac.in>
2024-09-23 21:35:02 +0530

    23 Sept '24 Solutions

---
+++ Practice/515C.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int ll
+#define x first
+#define y second
+#define mp make_pair
+#define pb push_back
+#define sqr(a) ((a) * (a))
+#define sz(a) int(a.size())
+#define all(a) a.begin(), a.end()
+#define forl(i,n) for(int i = 0; i < int(n); i++)
+#define forr(i,n) for(int i = n-1; i > -1; i--)
+#define fore(l, r) for(int i = int(l); i <= int(r); i++)
+#define fora(v,arr) for(auto v:arr)
+#define unm unordered_map
+#define uns unordered_set
+#define INT_MAX LLONG_MAX
+#define INT_MIN LLONG_MIN
+
+using ll = long long;
+typedef long double ld;
+using pii = pair<int, int>;
+typedef vector<long long> vi;
+
+// GET INTEGER ARRAY AS INPUT OF SIZE N
+vi getInts(int n) {
+    vi arr(n);
+    forl(i,n) cin>>arr[i];
+    return arr;
+}
+
+// UNION FIND CLASS
+class UnionFind {
+    vi parent, size;
+    public:
+    UnionFind(int n) {
+        this->parent.resize(n);
+        this->size.assign(n,1);
+        forl(i,n) parent[i] = i;
+    }
+
+    void Union(int a, int b) {
+        int pa = this->findParent(a), pb = this->findParent(b);
+        if(this->size[pa]>=this->size[pb]) {
+            this->parent[pb] = pa;
+            this->size[pa]++;
+        } else {
+            this->parent[pa] = pb;
+            this->size[pb]++;
+        }
+    }
+
+    int findParent(int a) {
+        if(this->parent[a]==a) return a;
+        return parent[a] = this->findParent(parent[a]);
+    }
+
+    vi getParentArr() {return this->parent;}
+};
+
+auto solve() {
+    int n;cin>>n;
+    unm<char,int> um;
+    while(n--) {
+        char e;cin>>e;
+        if(e=='0' or e=='1') continue;
+        else if(e=='2' or e=='3' or e=='5' or e=='7') um[e]++;
+        else if(e=='4') {
+            um['3']++;
+            um['2'] += 2;
+        } else if(e=='6') {
+            um['5']++;
+            um['3']++;
+        } else if(e=='8'){
+            um['7']++;
+            um['2'] += 3;
+        } else {
+            um['7']++;
+            um['3'] += 2;
+            um['2']++;
+        }
+    }
+    string ans = "";
+    forr(i,9) {
+        char e = '0'+i;
+        if(um.count(e)!=0) while(um[e]--) ans += e;
+    }
+    return ans;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t = 1;
+    // cin>>t;cin.clear()
+    while(t--) {
+        cout<<solve()<<endl;
+        // solve();
+    }
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+}
+
