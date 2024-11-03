017ef448d3f611b2f6dd980fcdbe85650bca23ef
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2024-11-04 00:01:49 +0530

    Update

--- Practice/478C.cpp
+++ Practice/478C.cpp
@@ -28,17 +28,31 @@ template<typename T> void print(T v) {return;}
 template<typename T> void print(vector<T> arr) {fa(i,arr) cout<<i<<" ";cout<<endl;}
 template<typename T1, typename T2> void print(vector<pair<T1,T2>> arr) {fa(i,arr) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
 template<typename T> void print(map<T,int> mp) {fa(i,mp) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
-template<typename T> vector<T> getArr(int n) {vector<T> arr(n);f(i,0,n,1) cin>>arr[i];return arr;}//GET ARRAY AS INPUT OF SIZE N
-template<typename T> map<T,int> getMapOfFreq(int n) {map<T,int> mp;f(i,0,n,1) {T e;cin>>e;mp[e]++;}return mp;}//GET ORDERED MAP AS INPUT WITH FREQUENCY OF EACH NUMBER
-template<typename T> set<T> getSet(int n) {set<T> st;f(i,0,n,1) {T e;cin>>e;st.insert(e);}return st;}//GET ORDERED SET AS INPUT
-int armin(vi &a) {int mn = INT_MAX;fa(i,a) mn = min(mn,i);return mn;}//GET MIN OF ARRAY
-int armax(vi &a) {int mx = INT_MIN;fa(i,a) mx = max(mx,i);return mx;}//GET MAX OF ARRAY
+
+// GET ARRAY AS INPUT OF SIZE N
+template<typename T> vector<T> getArr(int n) {vector<T> arr(n);f(i,0,n,1) cin>>arr[i];return arr;}
+
+// GET ORDERED MAP AS INPUT WITH FREQUENCY OF EACH NUMBER
+template<typename T> map<T,int> getMapOfFreq(int n) {map<T,int> mp;f(i,0,n,1) {T e;cin>>e;mp[e]++;}return mp;}
+
+// GET ORDERED SET AS INPUT
+template<typename T> set<T> getSet(int n) {set<T> st;f(i,0,n,1) {T e;cin>>e;st.insert(e);}return st;}
+
+// GET MIN OF ARRAY
+int armin(vi &a) {int mn = INT_MAX;fa(i,a) mn = min(mn,i);return mn;}
+
+// GET MAX OF ARRAY
+int armax(vi &a) {int mx = INT_MIN;fa(i,a) mx = max(mx,i);return mx;}
 
 auto solve() {
-    return 0;
+    vi a = getArr<int>(3);
+    sort(all(a));
+    if((a[0]+a[1])*2<=a[2]) return a[0]+a[1];
+    return accumulate(all(a),0ll)/3ll;
 }
 
-int32_t main() {ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);int t = 1;
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);int t = 1;
     // cin>>t;cin.clear();
     while(t--) {
         cout<<solve()<<endl;
