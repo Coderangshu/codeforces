38cb90ea49458089b46e1457ecd1d81f1a7884b8
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2024-11-09 13:28:27 +0530

    update

---
+++ Practice/363C.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int ll
+#define x first
+#define y second
+#define pb push_back
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define len(a) (long long)(a.size())
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define f(i, start, end, gap) for(long long  i=start; i<end; i+=gap)
+#define rf(i, start, end, gap) for(long long i=start; i>=end; i-=gap)
+#define fa(v,arr) for(auto v:arr)
+#define rfa(v,arr) for(auto v:reverse(all(arr)))
+#define INT_MAX LLONG_MAX
+#define INT_MIN LLONG_MIN
+
+using ll = long long;
+typedef long double ld;
+using pii = pair<int, int>;
+typedef vector<long long> vi;
+
+// PRINT ARRAY, MAP using this templates
+template<typename T> void print(T v) {return;}
+template<typename T> void print(vector<T> arr) {fa(i,arr) cout<<i<<" ";cout<<endl;}
+template<typename T1, typename T2> void print(vector<pair<T1,T2>> arr) {fa(i,arr) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
+template<typename T> void print(map<T,int> mp) {fa(i,mp) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
+template<typename T> vector<T> getArr(int n) {vector<T> arr(n);f(i,0,n,1) cin>>arr[i];return arr;}//GET ARRAY AS INPUT OF SIZE N
+template<typename T> map<T,int> getMapOfFreq(int n) {map<T,int> mp;f(i,0,n,1) {T e;cin>>e;mp[e]++;}return mp;}//GET ORDERED MAP AS INPUT WITH FREQUENCY OF EACH NUMBER
+template<typename T> set<T> getSet(int n) {set<T> st;f(i,0,n,1) {T e;cin>>e;st.insert(e);}return st;}//GET ORDERED SET AS INPUT
+int armin(vi &a) {int mn = INT_MAX;fa(i,a) mn = min(mn,i);return mn;}//GET MIN OF ARRAY
+int armax(vi &a) {int mx = INT_MIN;fa(i,a) mx = max(mx,i);return mx;}//GET MAX OF ARRAY
+
+auto solve() {
+    string s;cin>>s;
+    int i = 0, n = len(s), ans = 0;
+    string ns = "";
+    bool prev = false;
+    while(i<n) {
+        int j = i+1, cnt = 1;
+        while(j<n and s[i]==s[j]) j++,cnt++;
+        ns += s[i];
+        if(cnt>=2) {
+            if(prev) {
+                ans += cnt-1;
+                prev = false;
+            } else {
+                ans += cnt-2;
+                prev = true;
+                ns += s[i];
+            }
+        } else prev = false;
+        i = j;
+    }
+    return ns;
+}
+
+int32_t main() {ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);int t = 1;
+    // cin>>t;cin.clear();
+    while(t--) {
+        cout<<solve()<<endl;
+        // solve();
+    }
+    // cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+}
+
---
+++ Practice/490C.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int ll
+#define x first
+#define y second
+#define pb push_back
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define len(a) (long long)(a.size())
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define f(i, start, end, gap) for(long long  i=start; i<end; i+=gap)
+#define rf(i, start, end, gap) for(long long i=start; i>=end; i-=gap)
+#define fa(v,arr) for(auto v:arr)
+#define rfa(v,arr) for(auto v:reverse(all(arr)))
+#define INT_MAX LLONG_MAX
+#define INT_MIN LLONG_MIN
+
+using ll = long long;
+typedef long double ld;
+using pii = pair<int, int>;
+typedef vector<long long> vi;
+
+// PRINT ARRAY, MAP using this templates
+template<typename T> void print(T v) {return;}
+template<typename T> void print(vector<T> arr) {fa(i,arr) cout<<i<<" ";cout<<endl;}
+template<typename T1, typename T2> void print(vector<pair<T1,T2>> arr) {fa(i,arr) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
+template<typename T> void print(map<T,int> mp) {fa(i,mp) cout<<i.x<<" "<<i.y<<"; ";cout<<endl;}
+template<typename T> vector<T> getArr(int n) {vector<T> arr(n);f(i,0,n,1) cin>>arr[i];return arr;}//GET ARRAY AS INPUT OF SIZE N
+template<typename T> map<T,int> getMapOfFreq(int n) {map<T,int> mp;f(i,0,n,1) {T e;cin>>e;mp[e]++;}return mp;}//GET ORDERED MAP AS INPUT WITH FREQUENCY OF EACH NUMBER
+template<typename T> set<T> getSet(int n) {set<T> st;f(i,0,n,1) {T e;cin>>e;st.insert(e);}return st;}//GET ORDERED SET AS INPUT
+int armin(vi &a) {int mn = INT_MAX;fa(i,a) mn = min(mn,i);return mn;}//GET MIN OF ARRAY
+int armax(vi &a) {int mx = INT_MIN;fa(i,a) mx = max(mx,i);return mx;}//GET MAX OF ARRAY
+
+int gn(char c) {
+    return c-'0';
+}
+
+auto solve() {
+    string s;cin>>s;
+    int a,b;cin>>a>>b;
+    int n = len(s);
+    vi aRem(n), bRem(n);
+    int r = 0;
+    f(i,0,n,1) {
+        r = (10*r+gn(s[i]))%a;
+        if(!r) aRem[i] = 1;
+    }
+    r = 0; int pow10 = 1;
+    rf(i,n-1,0,1) {
+        r = (gn(s[i])*pow10+r)%b;
+        pow10 = (pow10*10)%b;
+        if(s[i]!='0' and !r) bRem[i] = 1;
+    }
+    // print(aRem);
+    // print(bRem);
+    f(i,0,n-1,1) {
+        if(aRem[i]==1 and bRem[i+1]==1) {
+            string s1 = s.substr(0,i+1), s2 = s.substr(i+1);
+            cout<<"YES\n"<<s1<<endl<<s2<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+    return;
+}
+
+int32_t main() {ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);int t = 1;
+    // cin>>t;cin.clear();
+    while(t--) {
+        // cout<<solve()<<endl;
+        solve();
+    }
+    // cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+}
+
